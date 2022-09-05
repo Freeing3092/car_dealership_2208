5bd461fcc9c4d7aaa11113e0c36cdeedb8136ec0
Rich Kaht <adamselene1776@protonmail.com>
2022-09-05 17:46:18 -0600

    Feature/Test: Add dealership methods

--- lib/dealership.rb
+++ lib/dealership.rb
@@ -14,4 +14,30 @@ class Dealership
   def add_car(car)
     @inventory << car
   end
+  
+  def has_inventory?
+    if inventory_count == 0
+      false
+    else
+      true
+    end
+  end
+  
+  def cars_by_make(make)
+    @inventory.find_all {|car| car.make == make}
+  end
+  
+  def total_value
+    inventory_cost = @inventory.map do |car|
+      car.total_cost
+    end
+    inventory_cost.sum
+  end
+  
+  def details
+    {
+      "total_value" => total_value,
+      "address" => @address
+    }
+  end
 end
