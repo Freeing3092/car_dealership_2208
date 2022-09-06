b6341b53a4f97484e8def5d062b055114f5c9e18
Rich Kaht <adamselene1776@protonmail.com>
2022-09-05 18:38:08 -0600

    Feature/Test: Add more dealership methods

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -106,6 +106,50 @@ RSpec.describe Dealership do
     
     expect(dealership.details).to eq({"total_value" => 156000, "address" => "123 Main Street"})
   end
+  it "#average_price_of_car return the average cost of inventory" do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    car_3 = Car.new("Toyota Tercel", 500, 48)
+    car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+    
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    dealership.add_car(car_3)
+    dealership.add_car(car_4)
+    expect(dealership.average_price_of_car).to eq("39,000")
+  end
+  
+  it "#cars_sorted_by_price return the inventory from lowest cost to highest" do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36) #54,000
+    car_2 = Car.new("Toyota Prius", 1000, 48) #48,000
+    car_3 = Car.new("Toyota Tercel", 500, 48) #24,000
+    car_4 = Car.new("Chevrolet Bronco", 1250, 24) #30,000
+    
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    dealership.add_car(car_3)
+    dealership.add_car(car_4)
+    
+    expect(dealership.cars_sorted_by_price).to eq([car_3, car_4, car_2, car_1])
+  end
+  
+  it "#inventory_hash returns a hash of cars from lowest cost to highest" do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36) #54,000
+    car_2 = Car.new("Toyota Prius", 1000, 48) #48,000
+    car_3 = Car.new("Toyota Tercel", 500, 48) #24,000
+    car_4 = Car.new("Chevrolet Bronco", 1250, 24) #30,000
+    
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    dealership.add_car(car_3)
+    dealership.add_car(car_4)
+    require "pry"; binding.pry
+    expect(dealership.inventory_hash).to eq()
+  end
+  
 end
 
 
